31f715f63f2ec2c86305b8180cbaea7e813036fa
Josh Rose <josh@etchapps.com>
2014-03-04 12:12:29 +0000

    Nicer tooltips

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,19 +5,15 @@
         username: 'headin-gs',
         type: 'loved',
         callback: function () {
-            var headings = $('.heading');
-
-            $.each(headings, function (i, v) {
-                console.log($(v));
-            });
+            $('.pen-tooltip').tooltip();
         },
         template: '' +
             '<article id="{{hash}}" class="heading">' +
             '<iframe src="{{urlFull}}" frameborder="0"></iframe>' +
             '<div class="meta">' +
-            '<a href="{{urlPen}}" title="View on Codepen"><i class="icon-download-2"></i></a>' +
+            '<a href="{{urlPen}}" data-toggle="tooltip" data-title="Edit on Codepen" class="pen-tooltip">' +
+            '<i class="icon-download-2"></i></a>' +
             '<a href="#{{hash}}"><i class="icon-twitter"></i></a>' +
-            '<i class="icon-info-sign"></i>' +
             '</div>' +
             '</article>'
     });
