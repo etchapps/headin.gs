399910d06e55751944fb77c3064c2145b4d26c83
Josh Rose <josh@etchapps.com>
2014-03-04 12:31:00 +0000

    Removing redundant css class.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,7 +11,7 @@
             '<article id="{{hash}}" class="heading">' +
                 '<iframe src="{{urlFull}}" frameborder="0"></iframe>' +
                 '<div class="meta">' +
-                    '<a href="{{urlPen}}" data-toggle="tooltip" data-title="View source" class="pen-tooltip">' +
+                    '<a href="{{urlPen}}" data-toggle="tooltip" data-title="View source">' +
                         '<i class="icon-download-2"></i>' +
                     '</a>' +
                     '<a href="https://twitter.com/intent/tweet?url=http%3A%2F%2Fheadin.gs%2F%23{{hash}}&via=etch&text={{title}}.%20Be%20more%20creative%20with%20your%20headings."' +
