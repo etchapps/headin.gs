07a6150ff7fd5d818e74a69dd939c794c9a6175d
Josh Rose <josh@etchapps.com>
2014-03-04 12:30:24 +0000

    Adding twitter intent code for following and tweeting.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,16 +5,20 @@
         username: 'headin-gs',
         type: 'loved',
         callback: function () {
-            $('.pen-tooltip').tooltip();
+            $('a[data-toggle]').tooltip();
         },
         template: '' +
             '<article id="{{hash}}" class="heading">' +
-            '<iframe src="{{urlFull}}" frameborder="0"></iframe>' +
-            '<div class="meta">' +
-            '<a href="{{urlPen}}" data-toggle="tooltip" data-title="Edit on Codepen" class="pen-tooltip">' +
-            '<i class="icon-download-2"></i></a>' +
-            '<a href="#{{hash}}"><i class="icon-twitter"></i></a>' +
-            '</div>' +
+                '<iframe src="{{urlFull}}" frameborder="0"></iframe>' +
+                '<div class="meta">' +
+                    '<a href="{{urlPen}}" data-toggle="tooltip" data-title="View source" class="pen-tooltip">' +
+                        '<i class="icon-download-2"></i>' +
+                    '</a>' +
+                    '<a href="https://twitter.com/intent/tweet?url=http%3A%2F%2Fheadin.gs%2F%23{{hash}}&via=etch&text={{title}}.%20Be%20more%20creative%20with%20your%20headings."' +
+                        ' data-toggle="tooltip" data-title="Tweet this heading">' +
+                        '<i class="icon-twitter"></i>' +
+                    '</a>' +
+                '</div>' +
             '</article>'
     });
 }(jQuery));
